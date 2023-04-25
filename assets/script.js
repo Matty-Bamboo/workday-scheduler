fd626757b5c978440368d9ed8f93e46473ca7679
Matt Miceli <126421060+Matty-Bamboo@users.noreply.github.com>
2023-04-25 09:37:09 -0700

    updated javaScript

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,72 @@
 // save emoji on saveBtn
 document.querySelector(".saveBtn").innerHTML="💾 Save"
 
-
+//var hour
 var eventsData
 var today = moment();
+
+// set textarea color based on time of day
+var setHourColor = function() {
+  var currentHour = today.hours();
+  console.log(currentHour);
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour-")[1]);      
+    console.log(timeId);
+    if (timeId < currentHour) {
+     $(this).addClass('past');
+    } 
+    else if (timeId === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
+}
 
 // set date & time 
 $('#currentDay').text(today.format('LLLL')); 
 
-// set textarea color based on time of day
-var setHourColor = function() {
-    var currentHour = today.hours();
-    console.log(currentHour);
-    $('.time-block').each(function () {
-        var timeId = parseInt($(this).attr('id').split("hour-")[1]);      
-        console.log(timeId);
-        if (timeId < currentHour) {
-          $(this).addClass('past');
-        } 
-        else if (timeId === currentHour) {
-          $(this).removeClass('past');
-          $(this).removeClass('future');
-          $(this).addClass('present');
-        }
-        else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
-          $(this).addClass('future');
-        }
-      });
-    }
-
 // retrieve items in local storage
-function loadStoredData() {
-    eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
-    if (!eventsData) {
-        eventsData = {
-        hour9: "",
-        hour10: "",
-        hour11: "",
-        hour12: "",
-        hour13: "", 
-        hour14: "",
-        hour15: "",
-        hour16: "",
-        hour17: "",
-        hour18: "",
-        };
-    }
+function loadStoredData () {
+  eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+  if (!eventsData) {
+      eventsData = {
+          hour9: "",
+          hour10: "",
+          hour11: "",
+          hour12: "",
+          hour13: "",
+          hour14: "",
+          hour15: "",
+          hour16: "",
+          hour17: "",
+          hour18: "",
+      };
   }
-function handleSaveClick(event) {
-   var hourBlock = $(event.target).parent();
-   var value = hourBlock.children("textarea").val();
-   var hour = hourBlock.attr('id').split("-")[1];
-
-   
-   localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
-   console.log(eventsData);
+    for (var i = 9; i < 18; i++) {
+      $("#hour-" + i + " textarea").val(eventsData["hour" + i]);
+    }
 }
+
+function handleSaveClick(event) {
+var hourBlock = $(event.target).parent();
+var value = hourBlock.children("textarea").val();
+var hour = hourBlock.attr('id').split("-")[1];
+
+eventsData["hour" + hour] = value;
+
+localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+}
+
+$('saveBtn').on('click', handleSaveClick);
 
 
 $(function() {
-    loadStoredData();
-    setHourColor();
+  loadStoredData();
+  setHourColor();
 });
-
-$('.saveBtn').on('click', handleSaveClick);
